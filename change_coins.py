07dd0338774c64a1bd49e8203898bca3e6c2db07
Raphael <pagliuca523@hotmail.com>
2021-02-14 21:17:00 +0000

    Program designed to calculate the value of some change in dollars.

---
+++ change_coins.py
@@ -0,0 +1,17 @@
+#Program designed to calculate the value of some change in dollars.
+#This program represents the total cash in cents.
+
+def main():
+
+    print("Change Counter\n")
+    print("Please enter the amount of each coin type. ")
+    quarters = int(input("Quarters: "))
+    dimes = int(input("Dimes: "))
+    nickels = int(input("Nickels: "))
+    pennies = int(input("Pennies: "))
+
+    total = quarters * 25 + dimes * 10 + nickels * 5 + pennies
+
+    print("The total values of your change is ${0}.{1:0>2}".format(total//100, total%100) ) 
+
+main()
